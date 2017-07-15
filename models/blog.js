d993c747e746367642187a8a8572fb2df9b8cee4
Shourya <mittalshourya97@gmail.com>
2017-07-16 02:19:22 +0530

    Adding more routes and Updating previous routes

--- models/blog.js
+++ models/blog.js
@@ -1,4 +1,4 @@
-var mymail;
+
 var router = require('express').Router(),
     nodemailer=require('nodemailer')
     connection= require('../connection'),
@@ -14,29 +14,79 @@ login=connection.seq.define('login',{
     },
     user_email:{
         type:sequelize.STRING,
+        allowNull:false,
+        primaryKey:true
+    },
+},
+{
+	freezeTableName:true,
+	timestamps:true,
+
+}),
+
+
+signup=connection.seq.define('signup',{
+    user_email:{
+        type:sequelize.STRING,
         allowNull:false
     },
     user_password:{
         type:sequelize.STRING,
         allowNull:false
     }
+
+
 },
 {
-	freezeTableName:true,
-	timestamps:true,
-
+    freezeTableName:true,
+    timestamps:true,
 })
 
 
 login.sync();
+signup.sync();
+
+
+//route to enter data in db to give access to users
+router.post('/signup',function(req,res){
+    var found=false;
+    data_body=req.body;
+    signup.find({
+        where:{
+            user_email:data_body.user_email
+        }
+    }).then((signup)=>{
+        if(signup)
+        {
+            found=false;
+        }
+        else
+        {
+            found=true;
+        }
+    }).then(()=>{
+        if(found==true)
+        {
+            signup.create({
+                user_email:data_body.user_email,
+                user_password:data_body.user_password
+            })
+
+            res.send("Data has been stored on the database"+"Go Green, Encash your trash!!");
+        }
+        else
+        {
+            res.send("The email id "+data_body.user_email+" exixts!, Please add some other email :)")
+        }
+
+    })
+})
 
 //for log in the user
 router.post('/login',function(req,res){
     data_body=req.body;
-    mymail=data_body.user_email;
     console.log(data_body);
-    var check=false;
-    login.find({
+    signup.find({
         where:{
             user_email:data_body.user_email
         }
@@ -49,7 +99,7 @@ router.post('/login',function(req,res){
         else
         {
             
-            res.send("You dont have permission");
+            res.send("Sorry you don't have access rights to this Blogging Platform, Please contact Admin!!");
         }
     })
 });
@@ -57,16 +107,12 @@ router.post('/login',function(req,res){
 router.post("/post",function(req,res){ 
     data_body=req.body;
     console.log(data_body);
-    console.log(mymail);
-    login.update({
-        user_post:data_body.user_post
-    },
-        {
-            where:{
-                user_email:mymail
-                }
-        })
-    res.send("Your post has been saved!");
+    login.create({
+        user_post:data_body.user_post,
+        user_email:data_body.user_email
+    })
+
+    res.send("Your content has been posted to Shuttlescrap.com!!");
 });
 
 
